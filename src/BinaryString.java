f38dba165dad8537c684718c59ac95ea1c89cabe
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-05 11:43:44 -0400

    Commit 4

--- src/BinaryString.java
+++ src/BinaryString.java
@@ -2,5 +2,9 @@ public class BinaryString {
     public static void main(String[] args) {
         int x = 2;
         System.out.println(Integer.toBinaryString(x));
+
+        x = x << 1;
+
+        System.out.println(x);
     }
 }
