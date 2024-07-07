47073b8946100f216ecdf32e49442973d2be5733
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-07 17:47:19 -0400

    Commit 13

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main{
     public static void main(String[] args) {
 
         int x = 150;
-        System.out.println("Binary version of 150:" +Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(x));
     }
-
 }
