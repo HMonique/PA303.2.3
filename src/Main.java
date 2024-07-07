5927a5164f8cb45c3997c22b08d777eaf2779302
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-07 17:51:14 -0400

    Commit 15

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main{
     public static void main(String[] args) {
+//
+//        int x = 150;
+//        System.out.println(Integer.toBinaryString(x));
+//
+//        x = x >> 2;
+//        System.out.println(x);
+//        System.out.println(Integer.toBinaryString(x));
 
-        int x = 150;
-        System.out.println(Integer.toBinaryString(x));
-
+        int x = 225;
         x = x >> 2;
         System.out.println(x);
+        System.out.println(Integer.toBinaryString(x));
 
     }
 }
