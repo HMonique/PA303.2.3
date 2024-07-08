b8c768f43f2a6303fb606fbd0f655a0689936a4e
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-07 22:46:57 -0400

    Commit 1

---
+++ src/Bitwise.java
@@ -0,0 +1,12 @@
+public class Bitwise {
+    public static void main(String[] args) {
+
+        int x = 7;
+        int y = 17;
+        int z;
+
+        z = x & y;
+        System.out.println(z);
+
+    }
+}
